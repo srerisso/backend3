05f221af1ea046cd05d83956c7f9dc6c0b8e4746
Jose M Gómez <jmgomezr@gmail.com>
2023-02-27 19:22:05 +0100

    Started to Fetch component data

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -7,7 +7,19 @@ class ComponentsController < ApplicationController
   end
 
   def show
+    @component = Component.find(params[:id])
+  end
 
+  def fetch
+    component = Component.find(params[:id])
+
+    if component.component_type == "components_cambios_changelog_proyectos"
+      changelog = Changelog.find(component.component_id)
+      @component_data = changelog
+    else
+      calculo = Calculo.find(component.component_id)
+      @component_data = calculo
+    end # endif
   end
   
 end
