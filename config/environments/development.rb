1ddd0a0e1e5da42dfef113f2738f0b0e2884330b
Jose M Gómez <jmgomezr@gmail.com>
2023-03-13 18:25:47 +0100

    Formatting CSS for cálculos in view Proyecto#show

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Enabling ngrok to outside connections.
+  config.hosts << "dc0c-185-228-53-80.eu.ngrok.io"
 end
