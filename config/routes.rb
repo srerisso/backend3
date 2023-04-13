e46a9345ea30abeb55d0e7eba9f54bbc95c400bb
Jose M Gómez <jmgomezr@gmail.com>
2023-04-13 13:28:08 +0200

    Fixed matrices_path not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :proyectos
-  resources :clientes
-  resources :calculos
+  # resources :proyectos
+  # resources :clientes
+  # resources :calculos
   get 'components/home'
   get 'components/index'
-  get 'component/home'
-  get 'component/index'
   root 'pages#index'
 
   resources :clientes, :proyectos, :chagelogs, :calculos
